85dfcced6c7884acc8a644c31e2c246078b7262a
Miguel Tornero <miguel.tornero.carrillo@gmail.com>
2024-06-09 10:37:33 -0600

    added delete_restaurant_by_id

--- views.py
+++ views.py
@@ -14,13 +14,35 @@ def get_restaurant_by_id(id: str):
     with create_session() as session:
         query_result = session.query(Restaurant).filter(Restaurant.id == id).first()
         if query_result == None:
-            return jsonify({"error": "not found"}), 400
+            return jsonify({"error": "not found"}), 404
         return jsonify(query_result)
     
 def update_restaurant_by_id(id: str):
     req_data = request.get_json(force=True)
     with create_session() as session:
-        session.query(Restaurant).filter(Restaurant.id == id).update(req_data, synchronize_session="fetch")
-        query_result = session.query(Restaurant).filter(Restaurant.id == id).first()
-        session.commit()
-        return jsonify(query_result)
+        query_result = session.query(Restaurant).filter(Restaurant.id == id).update(req_data, synchronize_session="fetch")
+        
+        try: 
+            session.commit()
+        except Exception as e:
+            return jsonify({"error": str(e)})
+        
+        status = 200
+        if query_result == 0:
+            status = 404
+        
+        return jsonify({"changed_rows": query_result}), status
+
+def delete_restaurant_by_id(id: str):
+    with create_session() as session:
+        query_result = session.query(Restaurant).filter(Restaurant.id == id).delete(synchronize_session="fetch")
+        try: 
+            session.commit()
+        except Exception as e:
+            return jsonify({"error": str(e)})
+        
+        status = 200
+        if query_result == 0:
+            status = 404
+        
+        return jsonify({"deleted_rows": query_result}), status
