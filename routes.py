2c706ae5a07644c6a0f2a3e65194dc65572f4e84
Miguel Tornero <miguel.tornero.carrillo@gmail.com>
2024-06-08 23:57:57 -0600

    added get_restaurant_by_id

--- routes.py
+++ routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
 
-from views import get_all_restaurants
+from views import get_all_restaurants, get_restaurant_by_id
 
 bp = Blueprint("routes", __name__)
 
 bp.add_url_rule("/restaurant", view_func=get_all_restaurants, methods=["GET"])
+bp.add_url_rule("/restaurant/<id>", view_func=get_restaurant_by_id, methods=["GET"])
--- views.py
+++ views.py
@@ -9,3 +9,11 @@ def get_all_restaurants(limit = DEFAULT_QUERY_LIMIT):
         query_results = session.query(Restaurant).limit(limit).all()
 
         return jsonify(query_results)
+
+def get_restaurant_by_id(id: str):
+    with create_session() as session:
+        query_result = session.query(Restaurant).filter(Restaurant.id == id).first()
+        if query_result == None:
+            return jsonify({"error": "not found"}), 400
+        return jsonify(query_result)
+        
