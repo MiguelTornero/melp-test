0083022a162f653f622ac87ba94130653994c554
Miguel Tornero <miguel.tornero.carrillo@gmail.com>
2024-06-09 01:14:53 -0600

    added update_restaurant_by_id

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, request
 
 from database import create_session
 from models import Restaurant
@@ -16,4 +16,11 @@ def get_restaurant_by_id(id: str):
         if query_result == None:
             return jsonify({"error": "not found"}), 400
         return jsonify(query_result)
-        
+    
+def update_restaurant_by_id(id: str):
+    req_data = request.get_json(force=True)
+    with create_session() as session:
+        session.query(Restaurant).filter(Restaurant.id == id).update(req_data, synchronize_session="fetch")
+        query_result = session.query(Restaurant).filter(Restaurant.id == id).first()
+        session.commit()
+        return jsonify(query_result)
