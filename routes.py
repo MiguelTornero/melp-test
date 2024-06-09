104607d1ea1ac0c67f4b33ad4abaa1c66d9d5f1a
Miguel Tornero <miguel.tornero.carrillo@gmail.com>
2024-06-09 12:55:54 -0600

    added create_restaurant

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-from views import get_all_restaurants, get_restaurant_by_id, update_restaurant_by_id, delete_restaurant_by_id
+from views import get_all_restaurants, get_restaurant_by_id, update_restaurant_by_id, delete_restaurant_by_id, create_restaurant
 
 bp = Blueprint("routes", __name__)
 
@@ -8,3 +8,4 @@ bp.add_url_rule("/restaurant", view_func=get_all_restaurants, methods=["GET"])
 bp.add_url_rule("/restaurant/<id>", view_func=get_restaurant_by_id, methods=["GET"])
 bp.add_url_rule("/restaurant/<id>", view_func=update_restaurant_by_id, methods=["PATCH"])
 bp.add_url_rule("/restaurant/<id>", view_func=delete_restaurant_by_id, methods=["DELETE"])
+bp.add_url_rule("/restaurant", view_func=create_restaurant, methods=["POST"])
--- views.py
+++ views.py
@@ -46,3 +46,41 @@ def delete_restaurant_by_id(id: str):
             status = 404
         
         return jsonify({"deleted_rows": query_result}), status
+
+def create_restaurant():
+    req_data : dict = request.get_json(force=True)
+    
+    r = Restaurant()
+
+    if "id" in req_data:
+        r.id = str(req_data["id"])
+    if "rating" in req_data:
+        r.rating = int(req_data["rating"])
+    if "name" in req_data:
+        r.name = str(req_data["name"])
+    if "site" in req_data:
+        r.site = str(req_data["site"])
+    if "email" in req_data:
+        r.email = str(req_data["email"])
+    if "phone" in req_data:
+        r.phone = str(req_data["phone"])
+    if "street" in req_data:
+        r.street = str(req_data["street"])
+    if "city" in req_data:
+        r.city = str(req_data["city"])
+    if "state" in req_data:
+        r.state = str(req_data["state"])
+    if "lat" in req_data:
+        r.lat = float(req_data["lat"])
+    if "lng" in req_data:
+        r.lng = float(req_data["lng"])
+    
+    with create_session() as sesssion:
+        sesssion.add(r)
+        try:
+            sesssion.commit()
+            sesssion.refresh(r)
+        except Exception as e:
+            return jsonify({"error": str(e)})
+        
+        return jsonify({"id": r.id})
